76f16dd9208cecc348498e84e08dd1bc7336311c
Milan Gruner <milangruner@gmail.com>
2018-05-04 16:19:40 +0200

    Add basic keyboard shortcuts (escape, f, p, r)

--- main.lua
+++ main.lua
@@ -1,10 +1,25 @@
 function love.load()
+    isRunning = true
     x, y = 100, 100
 end
 
 function love.update(dt)
+    if not isRunning then return end
+
     x = x + 10 * dt
     y = y + 10 * dt
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    elseif key == "f" then
+        love.window.setFullscreen(not love.window.getFullscreen())
+    elseif key == "r" then
+        love.load() -- reload game
+    elseif key == "p" then
+        isRunning = not isRunning
+    end
 end
 
 function love.draw()
