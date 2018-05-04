0f86147403b4cee6239cf0ccaaf5961a153cd959
Milan Gruner <milangruner@gmail.com>
2018-05-04 19:16:53 +0200

    Use hump.vector to shorten and fix applyGravityForces

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local vector = require 'libs.hump.vector'
+
 settings = {
     pixelsPerMeter = 35,
     movementSpeed = 15,
@@ -50,7 +52,7 @@ function makeCircle(x, y, r, isDynamic)
 	local shape = love.physics.newCircleShape(r)
 	local fixture = love.physics.newFixture(body, shape, 1)
     fixture:setDensity(1)
-    --fixture:setFriction(1)
+    fixture:setFriction(1)
     fixture:setRestitution(0)
     
     return body
@@ -94,35 +96,24 @@ function love.update(dt)
     applyGravityForces()
 end
 
-function getDistance(a, b)
-    local x, y = a.x - b.x, a.y - b.y
-    return math.sqrt(x*x + y*y)
-end
-
 function applyGravityForces()
     for _, object in ipairs(objects) do
-		local bx, by = object:getWorldCenter()
-		local bodyPosition = {x = bx, y = by}
+        local bodyPosition = vector(object:getWorldCenter())
 		
 		for _, planet in ipairs(planets) do
 			local shape = planet:getFixtures()[1]:getShape()
 			local radius = shape:getRadius()
-			local px, py = planet:getWorldCenter()
-			local planetPosition = {x = px, y = py}
+			local planetPosition = vector(planet:getWorldCenter())
 
-			local force = {x = 0, y = 0}
-			
-			force.x = force.x + bodyPosition.x - planetPosition.x
-			force.y = force.y + bodyPosition.y - planetPosition.y
-			
-			local forceMagnitude = getDistance({x = 0, y = 0}, force)
-			if forceMagnitude <= radius * 3 then
-				force.x = -force.x
-				force.y = -force.y
+			local bodyToPlanet = bodyPosition - planetPosition
+			local distanceToPlanet = bodyToPlanet:len()
+
+			if distanceToPlanet <= radius * 3 then
+                local force = -bodyToPlanet:clone()
 				
 				local sum = math.abs(force.x) + math.abs(force.y)
-				force.x = force.x * (1/sum * radius / forceMagnitude) * 2
-				force.y = force.y * (1/sum * radius / forceMagnitude) * 2
+				force.x = force.x * (1 / sum * radius / distanceToPlanet) * 2
+				force.y = force.y * (1 / sum * radius / distanceToPlanet) * 2
 				object:applyForce(force.x, force.y, bodyPosition.x, bodyPosition.y)
 			end
 		end
