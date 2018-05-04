77e2b9efe910a7e38a33200475fd8500b5c6f107
Milan Gruner <milangruner@gmail.com>
2018-05-04 16:16:56 +0200

    Make the circle move

--- main.lua
+++ main.lua
@@ -1,11 +1,12 @@
 function love.load()
-
+    x, y = 100, 100
 end
 
 function love.update(dt)
-
+    x = x + 10 * dt
+    y = y + 10 * dt
 end
 
 function love.draw()
-    love.graphics.circle('line', 100, 100, 50)
+    love.graphics.circle('line', x, y, 50)
 end
